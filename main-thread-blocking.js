0536e665eb913d75a4d0c3aa9cb62476f9cec02d
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2024-06-02 12:31:13 +0530

    Create main-thread-blocking.js

---
+++ main-thread-blocking.js
@@ -0,0 +1,14 @@
+console.log('Start')
+setTimeout(() => {
+  console.log('Timer ended')
+}, 5000)
+console.log('End')
+
+let startDate = new Date().getTime()
+let endDate = startDate;
+
+while(endDate < startDate + 10000){
+  endDate = new Date().getTime()
+}
+
+console.log('while expires')

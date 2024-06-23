d74205a5591edd11fb6ec59700066ef62aba5f08
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2024-06-23 16:40:10 +0530

    Update output-based-question-on-currying.js

--- output-based-question-on-currying.js
+++ output-based-question-on-currying.js
@@ -1,3 +1,13 @@
-let sum = (a) => (b) => b ? sum(a + b) : a;
+let sum = (a) => (b) => b ? sum(a + b) : a; // arrow function way of solving
+
+const sum2 = function(a){                  // function expression - old way
+  return function(b){
+    if(b){
+      return sum2(a + b);
+    }else{
+      return a;
+    }
+  }
+}
 
 console.log(sum(1)(2)(3)(4)());  // 10

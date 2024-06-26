1255101e15b6b6321d4153541dc742be3cbcdc91
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2024-06-26 18:55:11 +0530

    Update call-apply-bind-methods.js

--- call-apply-bind-methods.js
+++ call-apply-bind-methods.js
@@ -9,22 +9,22 @@ const getFullName = function(home) {
     console.log(this.firstname + ' ' + this.lastname, home);
   }
 
-getFullName.call(obj, 'KDP')
+getFullName.call(obj, 'KDP') // Saikumar Bolisetti KDP
 
 const obj2 = {
   firstname: 'Elon',
   lastname: 'Musk',
 }
 
-getFullName.call(obj2, 'USA')
+getFullName.call(obj2, 'USA')  // Elon Musk USA
 
 
 // apply method does the same thing but the args will be passed in an array
 
-getFullName.apply(obj, ['AUS'])
+getFullName.apply(obj, ['AUS']) // Saikumar Bolisetti AUS
 
 
 // bind method doesn't invoke the function but it will give you the function to execute later
 
-const bindMethod = getFullName.bind(obj, 'LA');
-bindMethod()
+const bindMethod = getFullName.bind(obj, 'LA'); 
+bindMethod()  // Saikumar Bolisetti LA

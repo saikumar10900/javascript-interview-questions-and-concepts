10db701f270135c8724586380beb04e8797d11e0
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2024-06-27 16:36:16 +0530

    Update closure.js

--- closure.js
+++ closure.js
@@ -12,3 +12,4 @@ result()
 // 10
 
 // function c formed a closure with its parent environment with b.
+// and the returned function will remember the value of b regardless of where it will get executed.

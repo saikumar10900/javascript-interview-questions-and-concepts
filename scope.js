3ff5746de5194d8b8c3f06f9edd503c0cb1f6e5d
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2024-07-02 21:24:48 +0530

    Update scope.js

--- scope.js
+++ scope.js
@@ -11,3 +11,4 @@ function a(){
 a();
 
 // Here, scope chain comes into picture where the function c can have the scope of it's parent and grand parent.
+// the function will search for the variable in its scope first, if it doesn't find then the control moves to the parent lexical scope, and repeats.
